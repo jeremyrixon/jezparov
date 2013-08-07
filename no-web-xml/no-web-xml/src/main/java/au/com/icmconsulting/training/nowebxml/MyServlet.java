fbf34420f9b331a85ea2a9853d511de14d2763d0
jezparov <jeremy@rixon.org>
2013-08-07 14:20:27 +1000

    Initial commit.

---
+++ no-web-xml/no-web-xml/src/main/java/au/com/icmconsulting/training/nowebxml/MyServlet.java
@@ -0,0 +1,19 @@
+package au.com.icmconsulting.training.nowebxml;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/")
+public class MyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().write("<h1>Hi!<h1>");
+	}
+
+}
