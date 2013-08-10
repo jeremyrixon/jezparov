bbf39dfcd12e3d368c136d76e892d96dff29aca1
jezparov <jeremy@rixon.org>
2013-08-10 21:48:31 +1000

    warnings

--- spring-data-example/spring-data-example/src/main/java/au/com/icmconsulting/training/repository/ForumGroupRepository.java
+++ spring-data-example/spring-data-example/src/main/java/au/com/icmconsulting/training/repository/ForumGroupRepository.java
@@ -1,11 +1,9 @@
 package au.com.icmconsulting.training.repository;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.rest.repository.annotation.RestResource;
 
 import au.com.icmconsulting.training.domain.ForumGroup;
 
-//@RestResource(path = "groups", rel = "groups")
 public interface ForumGroupRepository extends CrudRepository<ForumGroup, Long> {
 
 }
